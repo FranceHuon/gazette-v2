d2404804609e180b39092b140f7a3567bf96c607
FranceHuon <133089058+FranceHuon@users.noreply.github.com>
2026-01-07 18:24:51 +0100

    docs: add storybook documentation comment

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,6 +1,8 @@
 // eslint.config.js
 import antfu from '@antfu/eslint-config'
 
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+
 export default antfu(
   // Configures for antfu's config and global rules
   {
